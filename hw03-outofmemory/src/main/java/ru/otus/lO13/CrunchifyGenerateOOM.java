908dbfd7a105f1fcac7081c947a3bde6e3829ac1
Артем <artem.podstavkin@mail.ru>
2019-10-27 18:45:08 +0300

    ДЗ 3, Out Of Memory

--- hw03-outofmemory/src/main/java/ru/otus/lO13/CrunchifyGenerateOOM.java
+++ hw03-outofmemory/src/main/java/ru/otus/lO13/CrunchifyGenerateOOM.java
@@ -9,37 +9,56 @@ import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CrunchifyGenerateOOM {
+
+    private static final ExecutorService executor = Executors.newFixedThreadPool(5);
+
+    private static List<Long> allDuration = new ArrayList<>();
+    private static List<Integer> count = new ArrayList<>();
+    private static List<String> arrays = new ArrayList<>();
+    private static int countElement =0;
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting pid: " + ManagementFactory.getRuntimeMXBean().getName());
         switchOnMonitoring();
         long beginTime = System.currentTimeMillis();
-        CrunchifyGenerateOOM memoryTest = new CrunchifyGenerateOOM();
-        memoryTest.generateOOM();
-        System.out.println("time:" + (System.currentTimeMillis() - beginTime) / 1000);
+        try {
+            executor.submit(() -> {
+                try {
+                    try {
+                        generateOOM();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                } finally {
+                    countElement=arrays.size();
+                    arrays.clear(); // Очищаем память
+                }
+            }).get();
+        } catch (Throwable e) {
+            System.out.println("-----------------------------------------------------------------------------------------------------------\n"
+                    + "Общее время на сборку (" + getSum(allDuration) + " ms) " + " Количество сборок= " + count.size() + "\n" +
+                    "time (" + (System.currentTimeMillis() - beginTime) / 1000 + " s) " + "\n" +
+                    "Количество добавленных элементов: " + countElement + "\n" +
+                    "-----------------------------------------------------------------------------------------------------------");
+        }
+        executor.shutdownNow();
     }
 
-    public void generateOOM() throws Exception {
-        final List<String> arrays = new ArrayList<>();
-        for (long i = 0; i < Long.MAX_VALUE; i++) {
-            if (i != 0) {
-                arrays.add(arrays.get((int) (i - 1)) + String.valueOf(i) + "a");
-            } else {
-                arrays.add(i + "a");
-            }
-            Thread.sleep(30);
+    public static void generateOOM() throws OutOfMemoryError, InterruptedException {
+        String a = "a";
+        while (true) {
+            arrays.add(a);
+            a = a + "a";
+            Thread.sleep(10);
         }
     }
 
     private static void switchOnMonitoring() {
         List<GarbageCollectorMXBean> gcbeans = java.lang.management.ManagementFactory.getGarbageCollectorMXBeans();
-        List<Long> allDuration = new ArrayList<>();
-        List<Integer> countYang = new ArrayList<>();
-        List<Integer> countOld = new ArrayList<>();
-        List<Long> durationOld = new ArrayList<>();
-        List<Long> durationYang = new ArrayList<>();
         for (int i = 0; i < gcbeans.size(); i++) {
             System.out.println("GC name:" + gcbeans.get(i).getName());
             NotificationEmitter emitter = (NotificationEmitter) gcbeans.get(i);
@@ -55,18 +74,8 @@ public class CrunchifyGenerateOOM {
                     long duration = info.getGcInfo().getDuration();
 
                     System.out.println("start:" + startTime + " Name:" + gcName + ", action:" + gcAction + ", gcCause:" + gcCause + "(" + duration + " ms)");
-                    if (gcName.equals("Copy")) {
-                        countYang.add(1);
-                        durationYang.add(duration);
-                    } else {
-                        countOld.add(1);
-                        durationOld.add(duration);
-                    }
                     allDuration.add(duration);
-                    int count = countYang.size() + countOld.size();
-                    System.out.println("Общее время на сборку (" + getSum(allDuration) + " ms) " + " Количество сборок= " + count + "\n" +
-                            " Время на yang сбоки (" + getSum(durationYang) + " ms) " + " Количество yang сборок= " + countYang.size() + "\n" +
-                            " Время на old сбоки (" + getSum(durationOld) + " ms) " + " Количество old сборок= " + countOld.size());
+                    count.add(1);
                 }
             };
             emitter.addNotificationListener(listener, null, null);
