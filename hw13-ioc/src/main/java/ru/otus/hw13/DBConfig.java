c7a1756092ee95bcdcb007c612d5390aef409b9b
Артем <artem.podstavkin@mail.ru>
2020-02-19 19:52:31 +0300

    fix

--- hw13-ioc/src/main/java/ru/otus/hw13/DBConfig.java
+++ hw13-ioc/src/main/java/ru/otus/hw13/DBConfig.java
@@ -7,17 +7,13 @@ import org.springframework.context.annotation.Configuration;
 import ru.otus.hw13.api.model.User;
 import ru.otus.hw13.api.service.UserService;
 import ru.otus.hw13.hibernate.HibernateUtils;
-
-import java.util.HashMap;
-import java.util.Map;
+import ru.otus.hw13.web.startup.UsersCreator;
 
 @Configuration
 public class DBConfig {
     private static final String HIBERNATE_CFG_XML_FILE_RESOURCE = "/WEB-INF/config/hibernate.cfg.xml";
 
     private final ApplicationContext applicationContext;
-
-    private static Map<Long, User> users = new HashMap<>();
 
     public DBConfig(ApplicationContext applicationContext) {
         this.applicationContext = applicationContext;
@@ -29,19 +25,8 @@ public class DBConfig {
                 User.class);
     }
 
-    @Bean
-    public void userCreator() {
-        UserService userService = applicationContext.getBean(UserService.class);
-        users.put(1L, new User("Крис Гир", "user1", "11111"));
-        users.put(2L, new User("Ая Кэш", "user2", "11111"));
-        users.put(3L, new User("Десмин Боргес", "user3", "11111"));
-        users.put(4L, new User("Кетер Донохью", "user4", "11111"));
-        users.put(5L, new User("Стивен Шнайдер", "user5", "11111"));
-        users.put(6L, new User("Джанет Вэрни", "user6", "11111"));
-        users.put(7L, new User("Брэндон Смит", "user7", "11111"));
-        users.put(8L, new User("Artem", "admin", "admin"));
-        for (Map.Entry<Long, User> entry : users.entrySet()) {
-            userService.create(entry.getValue());
-        }
+    @Bean(initMethod = "createUsers")
+    public UsersCreator userCreator(UserService userService) {
+        return new UsersCreator(userService);
     }
 }
