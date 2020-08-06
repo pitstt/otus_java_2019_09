7fd8e34d37c6f6292649d9e24d7bb81bb3606db5
Подставкин Артем Алексеевич <aapodsta@mts.ru>
2020-08-06 12:15:03 +0300

    Домашнее задание (Собственный IoC контейнер) Цель: В процессе создания своего контекста понять как работает основная часть Spring framework. Обязательная часть:
    - Скачать заготовку приложения тренажера таблицы умножения из репозитория с примерами
    - В классе AppComponentsContainerImpl реализовать обработку, полученной в конструкторе конфигурации, основываясь на разметке аннотациями из пакета appcontainer.
    Так же необходимо реализовать методы getAppComponent
    - В итоге должно получиться работающее приложение. Менять можно только
    класс AppComponentsContainerImpl

---
+++ hw-di/src/main/java/ru/otus/appcontainer/AppComponentsContainerImpl.java
@@ -0,0 +1,63 @@
+package ru.otus.appcontainer;
+
+import ru.otus.appcontainer.api.AppComponent;
+import ru.otus.appcontainer.api.AppComponentsContainer;
+import ru.otus.appcontainer.api.AppComponentsContainerConfig;
+
+import java.lang.reflect.Method;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class AppComponentsContainerImpl implements AppComponentsContainer {
+
+    private final List<Object> appComponents = new ArrayList<>();
+    private final Map<String, Object> appComponentsByName = new HashMap<>();
+
+    public AppComponentsContainerImpl(Class<?> initialConfigClass) {
+        processConfig(initialConfigClass);
+    }
+
+    private void processConfig(Class<?> configClass) {
+        checkConfigClass(configClass);
+        List<Method> methods = Arrays.stream(configClass.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(AppComponent.class))
+                .sorted(Comparator.comparingInt(m -> m.getAnnotation(AppComponent.class).order()))
+                .collect(Collectors.toList());
+
+        for (Method method : methods) {
+            Object bean = createBean(method, configClass);
+            appComponents.add(bean);
+            appComponentsByName.put(method.getAnnotation(AppComponent.class).name(), bean);
+        }
+    }
+
+    private void checkConfigClass(Class<?> configClass) {
+        if (!configClass.isAnnotationPresent(AppComponentsContainerConfig.class)) {
+            throw new IllegalArgumentException(String.format("Given class is not config %s", configClass.getName()));
+        }
+    }
+
+    private Object createBean(Method method, Class<?> configClass) {
+        Object bean = null;
+        try {
+            bean = method.invoke(configClass.getDeclaredConstructor().newInstance(),
+                    Arrays.stream(method.getParameterTypes()).map(this::getAppComponent).toArray());
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return bean;
+    }
+
+    @Override
+    public <C> C getAppComponent(Class<C> componentClass) {
+        return (C) appComponents.stream()
+                .filter(componentClass::isInstance)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Нет такого элемента!!"));
+    }
+
+    @Override
+    public <C> C getAppComponent(String componentName) {
+        return (C) appComponentsByName.get(componentName);
+    }
+}
